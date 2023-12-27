2846002312865e697e14d7e0543a6bfcd74458fb
Максим Панчук <maksim.panchuk29@gmail.com>
2023-12-27 14:01:37 +0500

    core-notifications added

---
+++ services/backend-go/core-notifiactions/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+type Config struct {
+	HttpHost   string
+	HttpPort   string
+	RedisHost  string
+	RedisPort  string
+	RedisTopic string
+	DBUser     string
+	DBPassword string
+	DBHost     string
+	DBPort     string
+	DBName     string
+	TgToken    string
+}
